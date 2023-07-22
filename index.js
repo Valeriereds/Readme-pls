5cd59bf5b2620b8d3da8116f326f41d8a85c2d64
Valeriereds <valeriereds@gmail.com>
2023-07-22 15:34:16 -0500

    added more prompt questions and the functions to call them and input the data

--- index.js
+++ index.js
@@ -11,13 +11,16 @@
 
 // // Function call to initialize app
 // init();
+
 const inquirer = require('inquirer');
 
-inquirer
+function promptMe() {
+
+  inquirer
   .prompt([
     {
       type: 'input',
-      message: 'What is the title of your website',
+      message: 'What is the title of your website?',
       name: 'title',
     },
     {
@@ -27,7 +30,7 @@ inquirer
     },
     {
       type: 'input',
-      message: 'What are the steps required to install your project?',
+      message: 'What are the steps, if any, required to install your project?',
       name: 'installation',
     },
     {
@@ -36,13 +39,52 @@ inquirer
       name: 'usage'
     },
     {
-      type: 'list',
-      message: 'Please list your collaborators'
+      type: 'input',
+      message: 'Please list any and all of your collaborators here:',
+      name: 'collaborators'
+    },
+    {
+      type:'input',
+      message:'Please input how your contribution guidelines:',
+      name:'contributions'
+    },
+    {
+      type:'input',
+      message: 'Please input your test instructions here:',
+      name: 'testing'
+    },
+    {
+      type:'list',
+      message:'Which license would you like to use?',
+      name: 'license',
+      choices: [
+        'MIT', 'Apache', 'Boost Software', 'The Unlicense', 'Mozilla', 'GNU'
+      ],
+    },
+    {
+      type:'input',
+      message:'Please input your Github Username:',
+      name: 'github'
+    },
+    {
+      type:'input',
+      message:'Please input your email:',
+      name:'contact',
     }
   ])
-.then((response) => {
-  fs.writeFile('log.txt', process.argv[2], (err) =>
-  err ? console.error(err) : console.log('Success!')
-);
-}
+  .then((response) => 
+      writeToFile(markdown(response))
   );
+}
+
+function writeToFile(info) {
+  fs.appendFile('GenerateMe.md', `${info}\n`, (err) =>
+  err ? console.error(err) : console.log('Successfully generated ReadMe!')
+  );
+}
+
+function init() {
+  promptMe()
+}
+
+init()
